b9962db2c0213db35676b57ecfa87a32d27d1b54
t-imura <81124391+t-imura@users.noreply.github.com>
2025-03-05 11:56:49 +0900

    testcommit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("何か入力してください");
+            string name = Console.ReadLine();
+        }
+    }
+}
