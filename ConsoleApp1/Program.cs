0cb99851ba1b64cc070ab4bb378046358b8eca0d
t-imura <81124391+t-imura@users.noreply.github.com>
2025-03-07 15:57:02 +0900

    vs2022でのお試し更新

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -12,7 +12,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-            Console.WriteLine("");
+            Console.WriteLine("ぽえぽえ");
             Console.WriteLine("何か入力してください");
             string name = Console.ReadLine();
         }
